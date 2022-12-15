2954770edc7bd276a6aabea296cd5cfcaaadf542
Łukasz Lis <ll.lukasz.lis@gmail.com>
2022-12-15 21:37:32 +0100

    refactor: move settings to static class variables

--- FileGathererMethods.cs
+++ FileGathererMethods.cs
@@ -21,13 +21,51 @@ using System.Linq;
 using System.Text;
 //
 using Microsoft.Win32;
-using Spire.Pdf.Exporting.XPS.Schema;
+using System.Diagnostics;
 
 namespace PGNiG_FileProcessor
 {
     public class FileGatherer
     {
-        const string RegisterKey = "HKEY_CURRENT_USER\\Software\\LukardiSettings";
+        private static readonly string RegisterKey = "SOFTWARE\\LukardiSettings";
+        private static readonly string RegisterValueKey = "CurrentFVNumber";
+        //
+        private static readonly string SourceInboxFolderName = "Do Importu";
+        private static readonly string DestinationInboxFolderName = "Zaimportowane";
+        //
+        private static readonly string SMTPServer = ConfigurationManager.AppSettings.Get("SMTPServer");
+        private static readonly int SMTPPort = 25;
+        private static readonly string IMAPServer = "imap.pgnig.pl";
+        private static readonly int IMAPPort = 143;
+        //
+        private static readonly string NetworkFolder = ConfigurationManager.AppSettings.Get("NetworkFolder");
+        private static readonly string ProcessedZIPFiles = ConfigurationManager.AppSettings.Get("ProcessedZIPFiles");
+        private static readonly string InputClassificationFolder = ConfigurationManager.AppSettings.Get("InputClassificationFolder");
+        private static readonly string InitialFolder = ConfigurationManager.AppSettings.Get("InitialFolder");
+        private static readonly string CredentialPairName = ConfigurationManager.AppSettings.Get("CredentialPairName");
+        private static readonly List<string> ErrorMailReceivers = ConfigurationManager.AppSettings.Get("ErrorMailReceivers").Split(';').ToList();
+        private static readonly string OutputClassificationFolder = ConfigurationManager.AppSettings.Get("OutputClassificationFolder");
+        private static readonly string CompleteFVs = ConfigurationManager.AppSettings.Get("CompleteFVs");
+        private static readonly string Barcoder = ConfigurationManager.AppSettings.Get("Barcoder");
+        //
+
+        private static readonly string[] extensions = {
+                ".docx#",
+                ".doc#",
+                ".xls#",
+                ".xlsx#",
+                ".rtf#",
+                ".ods#",
+                ".odt#", 
+                //
+                ".docx",
+                ".doc",
+                ".xls",
+                ".xlsx",
+                ".rtf",
+                ".ods",
+                ".odt"
+            };
 
         public static void Run()
         {
@@ -58,9 +96,6 @@ namespace PGNiG_FileProcessor
 
         public static void CollectNetworkFiles()
         {
-            string NetworkFolder = ConfigurationManager.AppSettings.Get("NetworkFolder");
-            string ProcessedZIPFilesFolder = ConfigurationManager.AppSettings.Get("ProcessedZIPFiles");
-
             try
             {
                 string[] array2 = Directory.GetFiles(NetworkFolder, "*.zip");
@@ -68,27 +103,20 @@ namespace PGNiG_FileProcessor
                 {
                     GetAttachmentsFromZIPFile(zipfile);
                     //File.Delete(zipfile);
-                    File.Move(zipfile, ProcessedZIPFilesFolder + @"\" + System.IO.Path.GetFileName(zipfile));
-
+                    File.Move(zipfile, ProcessedZIPFiles + @"\" + Path.GetFileName(zipfile));
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("There was an error: {0}", ex.Message);
             }
-
-
         }
 
         public static void GetAttachmentsFromZIPFile(string zipfile)
         {
-            string InputClassificationFolder = ConfigurationManager.AppSettings.Get("InputClassificationFolder");
-            string InitialFolder = ConfigurationManager.AppSettings.Get("InitialFolder");
-
             string path = $"{InitialFolder}\\temp";
             Directory.CreateDirectory(path);
             ZipFile.ExtractToDirectory(zipfile, path);
-
             string[] array = Directory.GetFiles(path, "*.msg");
             Storage.Message messagefile = new Storage.Message(array[0], FileAccess.ReadWrite);
             string date = messagefile.Headers.DateSent.ToLocalTime().ToString("yyyy'-'MM'-'dd'T'HH'-'mm'-'ss");
@@ -128,24 +156,26 @@ namespace PGNiG_FileProcessor
             string MailBody = message;
             PdfDocument pdf = new PdfDocument();
             PdfPageBase page = pdf.Pages.Add();
-            PdfFont font = new PdfFont(PdfFontFamily.TimesRoman, 11);
-            PdfTextLayout textLayout = new PdfTextLayout();
-            textLayout.Break = PdfLayoutBreakType.FitPage;
-            textLayout.Layout = PdfLayoutType.Paginate;
-            PdfStringFormat format = new PdfStringFormat();
-            //format.Alignment = PdfTextAlignment.Justify;
-            format.LineSpacing = 20f;
-            PdfTextWidget textWidget = new PdfTextWidget(MailBody, new PdfTrueTypeFont(new Font("Arial", 11), true), PdfBrushes.Black);
-            textWidget.StringFormat = format;
+            PdfTextLayout textLayout = new PdfTextLayout
+            {
+                Break = PdfLayoutBreakType.FitPage,
+                Layout = PdfLayoutType.Paginate
+            };
+            PdfStringFormat format = new PdfStringFormat
+            {
+                LineSpacing = 20f
+            };
+            PdfTextWidget textWidget = new PdfTextWidget(MailBody, new PdfTrueTypeFont(new Font("Arial", 11), true), PdfBrushes.Black)
+            {
+                StringFormat = format
+            };
             RectangleF bounds = new RectangleF(new PointF(10, 25), page.Canvas.ClientSize);
             textWidget.Draw(page, bounds, textLayout);
-
-
             pdf.SaveToFile(file, FileFormat.PDF);
             return file;
         }
 
-        public static void GetAttachments(MimeMessage message, string date, string path)
+        public static void GetAttachments(MimeMessage message, string path)
         {
             foreach (var attachment in message.Attachments)
             {
@@ -176,18 +206,16 @@ namespace PGNiG_FileProcessor
 
         public static void DownloadMessages()
         {
-            string InitialFolder = ConfigurationManager.AppSettings.Get("InitialFolder");
             using (var client = new ImapClient(new ProtocolLogger("imap.log")))
             {
                 MyWebClient webClient = new MyWebClient();
                 try
                 {
-                    client.Connect("imap.pgnig.pl", 143, SecureSocketOptions.StartTls);
-                    string CredentialPairName = ConfigurationManager.AppSettings.Get("CredentialPairName");
+                    client.Connect(IMAPServer, IMAPPort, SecureSocketOptions.Auto);
                     client.Authenticate(webClient.GetUsername(CredentialPairName), webClient.GetPassword(CredentialPairName));
                     client.Inbox.Open(FolderAccess.ReadWrite);
                     var folder = client.Inbox.GetSubfolders();
-                    var subfolder = client.Inbox.GetSubfolder("Do Importu");
+                    var subfolder = client.Inbox.GetSubfolder(SourceInboxFolderName);
                     subfolder.Open(FolderAccess.ReadWrite);
                     var items = subfolder.Fetch(0, -1, MessageSummaryItems.UniqueId | MessageSummaryItems.Size | MessageSummaryItems.Flags);
                     foreach (var item in items)
@@ -200,8 +228,7 @@ namespace PGNiG_FileProcessor
                         {
                             Directory.CreateDirectory(path);
                             mailPDF = CreateMailBodyPDFFile(message.Date.ToLocalTime() + Environment.NewLine + message.Subject + Environment.NewLine + message.TextBody, path);
-                            GetAttachments(message, date, path);
-                            string InputClassificationFolder = ConfigurationManager.AppSettings.Get("InputClassificationFolder");
+                            GetAttachments(message, path);
                             MoveFolderForClassification(path, InputClassificationFolder, date);
                         }
                         catch (Exception msgEx)
@@ -215,7 +242,7 @@ namespace PGNiG_FileProcessor
                             {
                                 Directory.Delete(path, true);
                             }
-                            subfolder.MoveTo(item.UniqueId, client.Inbox.GetSubfolder("Zaimportowane"));
+                            subfolder.MoveTo(item.UniqueId, client.Inbox.GetSubfolder(DestinationInboxFolderName));
                         }
                     }
                 }
@@ -236,16 +263,12 @@ namespace PGNiG_FileProcessor
                 MyWebClient webClient = new MyWebClient();
                 try
                 {
-                    client.Connect(ConfigurationManager.AppSettings.Get("SMTPServer"), 25, SecureSocketOptions.Auto);
-                    string CredentialPairName = ConfigurationManager.AppSettings.Get("CredentialPairName");
+                    client.Connect(SMTPServer, SMTPPort, SecureSocketOptions.Auto);
                     client.Authenticate(webClient.GetUsername(CredentialPairName), webClient.GetPassword(CredentialPairName));
 
                     var message = new MimeMessage();
                     message.From.Add(new MailboxAddress("Service Error", "EfakturaODtest@pgnig.pl"));
-                    //
-                    var emails = ConfigurationManager.AppSettings.Get("ErrorMailReceivers").Split(';').ToList();
-                    emails.RemoveAll(s => string.IsNullOrEmpty(s));
-                    foreach (var email in emails)
+                    foreach (var email in ErrorMailReceivers)
                     {
                         message.To.Add(new MailboxAddress(email, email));
                     }
@@ -265,7 +288,7 @@ namespace PGNiG_FileProcessor
                             Content = new MimeContent(File.OpenRead(mailPDF)),
                             ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
                             ContentTransferEncoding = ContentEncoding.Base64,
-                            FileName = System.IO.Path.GetFileName(mailPDF)
+                            FileName = Path.GetFileName(mailPDF)
                         };
                         message.Body = new Multipart("mixed")
                         {
@@ -295,14 +318,12 @@ namespace PGNiG_FileProcessor
                 try
                 {
                     client.Connect("imap.pgnig.pl", 143, SecureSocketOptions.StartTls);
-
-                    string CredentialPairName = ConfigurationManager.AppSettings.Get("CredentialPairName");
                     client.Authenticate(webClient.GetUsername(CredentialPairName), webClient.GetPassword(CredentialPairName));
-                    client.Inbox.Open(MailKit.FolderAccess.ReadWrite);
+                    client.Inbox.Open(FolderAccess.ReadWrite);
                     var folder = client.Inbox.GetSubfolders();
 
-                    var subfolder = client.Inbox.GetSubfolder("Do Importu");
-                    subfolder.Open(MailKit.FolderAccess.ReadWrite);
+                    var subfolder = client.Inbox.GetSubfolder(SourceInboxFolderName);
+                    subfolder.Open(FolderAccess.ReadWrite);
 
                     subfolder.MoveTo(msgsummary.UniqueId, client.Inbox);
                     ////
@@ -319,7 +340,6 @@ namespace PGNiG_FileProcessor
 
         public static void CleanUpLockLibreFiles(string path)
         {
-            string[] extensions = { ".docx#", ".doc#", ".xls#", ".xlsx#", ".rtf#", ".ods#", ".odt#", ".docx", ".doc", ".xls", ".xlsx", ".rtf", ".ods", ".odt" };
             string[] officefiles = Directory.GetFiles(path, "*.*")
                 .Where(f => extensions.Contains(new FileInfo(f).Extension.ToLower())).ToArray();
 
@@ -333,7 +353,6 @@ namespace PGNiG_FileProcessor
         {
             List<string> FVs = new List<string>();
             List<string> Attachments = new List<string>();
-            string OutputClassificationFolder = ConfigurationManager.AppSettings.Get("OutputClassificationFolder");
             string[] pdfarray = Directory.GetFiles(OutputClassificationFolder, "*.pdf");
 
             foreach (string pdf in pdfarray)
@@ -347,10 +366,12 @@ namespace PGNiG_FileProcessor
                     Attachments.Add(pdf);
                 }
             }
+            //TODO: Verify multiple FVs with only one attachements array
             foreach (string FV in FVs)
             {
                 MergePDF(FV, Attachments);
             }
+            //TODO: All files will be removed even when FVs wont be found
             CleanupOutputClassificationFolder(pdfarray);
         }
 
@@ -364,13 +385,12 @@ namespace PGNiG_FileProcessor
 
         public static void MergePDF(string FV, List<string> Attachments)
         {
-            string FinalFVsDirectory = ConfigurationManager.AppSettings.Get("CompleteFVs");
 
             List<string> PDF_To_Merge = new List<string>();
             string bid = FV.Substring(GetNthIndex(FV, '_', 1) + 1);
             bid = bid.Split('_')[0];
             PDF_To_Merge.Add(FV);
-            ///////////////////////
+
             foreach (string attachment in Attachments)
             {
                 if (attachment.Contains(bid))
@@ -380,38 +400,47 @@ namespace PGNiG_FileProcessor
             }
 
             PdfDocumentBase mergedPDF = PdfDocument.MergeFiles(PDF_To_Merge.ToArray());
-            string filename = System.IO.Path.GetFileName(FV);
-            mergedPDF.Save(FinalFVsDirectory + filename);
-
+            string filename = Path.GetFileName(FV);
+            var file = Path.Combine(CompleteFVs, filename);
+            mergedPDF.Save(file);
             int year = DateTime.Today.Year;
             string FVnr = $"E{year}5" + GetNextFVNumber().ToString("D6");
-            ///Start Barcoder
-            System.Diagnostics.Process.Start(ConfigurationManager.AppSettings.Get("Barcoder"), $"\"{FinalFVsDirectory}{filename}\" {FVnr} barcode \"{FinalFVsDirectory}{filename}");
 
+            RunBarcode(file, FVnr);
+        }
+
+        public static void RunBarcode(string file, string number)
+        {
+            Process.Start(Barcoder, "\"" + string.Join("\" \"", new List<string>() {
+                file,
+                number,
+                "barcode",
+                file
+            }) + "\"").WaitForExit();
         }
 
         public static long GetNextFVNumber()
         {
             long FVNumber;
-            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\LukardiSettings");
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(RegisterKey);
             if (key != null)
             {
-                if (key.GetValue("CurrentFVNumber") != null)
+                if (key.GetValue(RegisterValueKey) != null)
                 {
-                    FVNumber = (long)key.GetValue("CurrentFVNumber");
+                    FVNumber = (long)key.GetValue(RegisterValueKey);
                     FVNumber++;
-                    Registry.SetValue(RegisterKey, "CurrentFVNumber", FVNumber, RegistryValueKind.QWord);
+                    Registry.SetValue($"HKEY_CURRENT_USER\\{RegisterKey}", RegisterValueKey, FVNumber, RegistryValueKind.QWord);
                 }
                 else
                 {
-                    Registry.SetValue(RegisterKey, "CurrentFVNumber", 1, RegistryValueKind.QWord);
-                    FVNumber = (long)key.GetValue("CurrentFVNumber");
+                    Registry.SetValue($"HKEY_CURRENT_USER\\{RegisterKey}", RegisterValueKey, 1, RegistryValueKind.QWord);
+                    FVNumber = (long)key.GetValue(RegisterValueKey);
                 }
                 key.Close();
             }
             else
             {
-                Registry.CurrentUser.CreateSubKey(@"SOFTWARE\LukardiSettings");
+                Registry.CurrentUser.CreateSubKey(RegisterKey);
                 return 1;
             }
             return FVNumber;
